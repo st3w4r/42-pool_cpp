d16d89ea83aacf420bcf6ed5d8756e65d080570d
Yanael Barbier <ybarbier@student.42.fr>
2015-06-17 12:20:11 +0200

    Send ex01 in progress

---
+++ d02/ex01/Fixed.class.cpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.class.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/06/17 10:46:11 by ybarbier          #+#    #+#             */
+/*   Updated: 2015/06/17 10:46:12 by ybarbier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.class.hpp"
+
+Fixed::Fixed(void) : _n(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return;
+}
+
+Fixed::Fixed(Fixed const & src)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return;
+}
+
+Fixed::Fixed(int const n) : _n(n)
+{
+	std::cout << "Int constructor called" << std::endl;
+	return:
+}
+
+Fixed::Fixed(float const n) : _n(n)
+{
+	std::cout << "Float constructor called" << std::endl;
+	return:
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return;
+}
+
+Fixed & Fixed::operator=(Fixed const & rhs)
+{
+	std::cout << "Assignation operator called" << std::endl;
+
+	this->_n = rhs.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_n;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->_n = raw;
+	return;
+}
---
+++ d02/ex01/Fixed.class.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.class.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ybarbier <ybarbier@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/06/17 10:46:08 by ybarbier          #+#    #+#             */
+/*   Updated: 2015/06/17 10:46:09 by ybarbier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_CLASS_HPP
+# define FIXED_CLASS_HPP
+
+#include <iostream>
+
+class Fixed {
+
+	private:
+		int _n;
+		static const int _fractionalBits = 8;
+
+	public:
+		Fixed(void);
+		Fixed(Fixed const & src);
+		Fixed(int const n);
+		Fixed(float const n);
+		~Fixed(void);
+
+		Fixed & operator=(Fixed const & rhs);
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+
+#endif /* end of include guard: FIXED_CLASS_HPP */
